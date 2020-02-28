6c563b91057217eed8a50eae977a7552c0554d9f
hideyuk1-jp <hh11028+github@gmail.com>
2020-02-28 14:02:03 +0900

    d 7

--- src/index.js
+++ src/index.js
@@ -4,18 +4,16 @@ import ReactDOM from "react-dom";
 const App = props => {
   const [value, setValue] = useState(10);
 
-  const hello = who => {
-    return () => {
-      console.log("hello", who);
-    };
+  const setToValue = newValue => () => {
+    setValue(newValue);
   };
 
   return (
     <div>
       {value}
-      <button onClick={hello("world")}>button</button>
-      <button onClick={hello("react")}>button</button>
-      <button onClick={hello("function")}>button</button>
+      <button onClick={setToValue(1000)}>thousand</button>
+      <button onClick={setToValue(0)}>reset</button>
+      <button onClick={setToValue(value + 1)}>increment</button>
     </div>
   );
 };
