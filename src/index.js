1ab57cc8c7a11882df5ccfe319cc1af69ef28ec2
hideyuk1-jp <hh11028+github@gmail.com>
2020-02-28 13:56:38 +0900

    d 6

--- src/index.js
+++ src/index.js
@@ -1,40 +1,21 @@
 import React, { useState } from "react";
 import ReactDOM from "react-dom";
 
-const History = props => {
-  if (props.allClicks.length === 0) {
-    return <div>the app is used by pressing the buttons</div>;
-  }
-
-  return <div>button press history: {props.allClicks.join(" ")}</div>;
-};
-
-const Button = ({ onClick, text }) => <button onClick={onClick}>{text}</button>;
-
 const App = props => {
-  const [left, setLeft] = useState(0);
-  const [right, setRight] = useState(0);
-  const [allClicks, setAll] = useState([]);
+  const [value, setValue] = useState(10);
 
-  const handleLeftClick = () => {
-    setAll(allClicks.concat("L"));
-    setLeft(left + 1);
-  };
-
-  const handleRightClick = () => {
-    setAll(allClicks.concat("R"));
-    setRight(right + 1);
+  const hello = who => {
+    return () => {
+      console.log("hello", who);
+    };
   };
 
   return (
     <div>
-      <div>
-        {left}
-        <Button onClick={handleLeftClick} text="left" />
-        <Button onClick={handleRightClick} text="right" />
-        {right}
-        <History allClicks={allClicks} />
-      </div>
+      {value}
+      <button onClick={hello("world")}>button</button>
+      <button onClick={hello("react")}>button</button>
+      <button onClick={hello("function")}>button</button>
     </div>
   );
 };
