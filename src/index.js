f99e7e520d01d350b500b3a486f900d6a45dbd99
hideyuk1-jp <hh11028+github@gmail.com>
2020-02-28 14:25:10 +0900

    d Exercises 1.6

--- src/index.js
+++ src/index.js
@@ -1,26 +1,22 @@
 import React, { useState } from "react";
 import ReactDOM from "react-dom";
 
-// This is the right place to define a component
-const Button = props => (
-  <button onClick={props.handleClick}>{props.text}</button>
-);
-
-const Display = props => <div>{props.value}</div>;
-
-const App = props => {
-  const [value, setValue] = useState(10);
-
-  const setToValue = newValue => {
-    setValue(newValue);
-  };
+const App = () => {
+  // save clicks of each button to own state
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
 
   return (
     <div>
-      <Display value={value} />
-      <Button handleClick={() => setToValue(1000)} text="thousand" />
-      <Button handleClick={() => setToValue(0)} text="reset" />
-      <Button handleClick={() => setToValue(value + 1)} text="increment" />
+      <h1>give feedback</h1>
+      <button onClick={() => setGood(good + 1)}>good</button>
+      <button onClick={() => setNeutral(neutral + 1)}>neutral</button>
+      <button onClick={() => setBad(bad + 1)}>bad</button>
+      <h1>statistics</h1>
+      <div>good {good}</div>
+      <div>neutral {neutral}</div>
+      <div>bad {bad}</div>
     </div>
   );
 };
