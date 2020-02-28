db26e5525b3e9ec9bb106d23b5028b6ca7f403fe
hideyuk1-jp <hh11028+github@gmail.com>
2020-02-28 13:13:18 +0900

    d 4

--- src/index.js
+++ src/index.js
@@ -1,6 +1,14 @@
 import React, { useState } from "react";
 import ReactDOM from "react-dom";
 
+const History = props => {
+  if (props.allClicks.length === 0) {
+    return <div>the app is used by pressing the buttons</div>;
+  }
+
+  return <div>button press history: {props.allClicks.join(" ")}</div>;
+};
+
 const App = props => {
   const [left, setLeft] = useState(0);
   const [right, setRight] = useState(0);
@@ -23,7 +31,7 @@ const App = props => {
         <button onClick={handleLeftClick}>left</button>
         <button onClick={handleRightClick}>right</button>
         {right}
-        <p>{allClicks.join(" ")}</p>
+        <History allClicks={allClicks} />
       </div>
     </div>
   );
