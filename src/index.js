0303d828ddb77e96e89635ee3cb836d7a95f2915
hideyuk1-jp <hh11028+github@gmail.com>
2020-02-28 11:29:13 +0900

    d

--- src/index.js
+++ src/index.js
@@ -2,13 +2,18 @@ import React, { useState } from "react";
 import ReactDOM from "react-dom";
 
 const App = props => {
-  const [counter, setCounter] = useState(0);
+  const [left, setLeft] = useState(0);
+  const [right, setRight] = useState(0);
 
   return (
-    <>
-      <div>{counter}</div>
-      <button onClick={setCounter(counter + 1)}>plus</button>
-    </>
+    <div>
+      <div>
+        {left}
+        <button onClick={() => setLeft(left + 1)}>left</button>
+        <button onClick={() => setRight(right + 1)}>right</button>
+        {right}
+      </div>
+    </div>
   );
 };
 
