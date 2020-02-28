c2b3a296422725b623d25f495f956762e355324f
hideyuk1-jp <hh11028+github@gmail.com>
2020-02-28 11:32:08 +0900

    d 1

--- src/index.js
+++ src/index.js
@@ -2,16 +2,34 @@ import React, { useState } from "react";
 import ReactDOM from "react-dom";
 
 const App = props => {
-  const [left, setLeft] = useState(0);
-  const [right, setRight] = useState(0);
+  const [clicks, setClicks] = useState({
+    left: 0,
+    right: 0
+  });
+
+  const handleLeftClick = () => {
+    const newClicks = {
+      left: clicks.left + 1,
+      right: clicks.right
+    };
+    setClicks(newClicks);
+  };
+
+  const handleRightClick = () => {
+    const newClicks = {
+      left: clicks.left,
+      right: clicks.right + 1
+    };
+    setClicks(newClicks);
+  };
 
   return (
     <div>
       <div>
-        {left}
-        <button onClick={() => setLeft(left + 1)}>left</button>
-        <button onClick={() => setRight(right + 1)}>right</button>
-        {right}
+        {clicks.left}
+        <button onClick={handleLeftClick}>left</button>
+        <button onClick={handleRightClick}>right</button>
+        {clicks.right}
       </div>
     </div>
   );
